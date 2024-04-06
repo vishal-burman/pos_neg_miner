10857e59311529e4330522e57810b660cb7a0054
Vishal Burman <vishal.a.burman23@gmail.com>
2024-04-06 18:19:10 +0530

    black + isort fix

--- pos_neg_miner/miner.py
+++ pos_neg_miner/miner.py
@@ -1,9 +1,15 @@
 from typing import List
+
 from utils import validate_queries_and_candidates
 
+
 class Miner:
-    def __init__(self,):
+    def __init__(
+        self,
+    ):
         pass
-    
+
     def __call__(self, queries: List[str], candidates: List[str]):
-        queries, candidates = validate_queries_and_candidates(queries=queries, candidates=candidates)
+        queries, candidates = validate_queries_and_candidates(
+            queries=queries, candidates=candidates
+        )
--- pos_neg_miner/utils.py
+++ pos_neg_miner/utils.py
@@ -1,12 +1,16 @@
 from typing import List, Tuple
 
+
 def sanitate_text(text: str) -> str:
     return text.strip()
 
-def validate_queries_and_candidates(queries: List[str], candidates: List[str]) -> Tuple[List[str], List[str]]:
+
+def validate_queries_and_candidates(
+    queries: List[str], candidates: List[str]
+) -> Tuple[List[str], List[str]]:
     queries = list(map(sanitate_text, set(queries)))
     candidates = list(map(sanitate_text, set(candidates)))
     if not queries or not candidates:
         raise ValueError(f"Candidates or Queries cannot be zero!")
-    
+
     return queries, candidates
