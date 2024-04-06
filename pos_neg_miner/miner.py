d1b9cc20ddb170c19767c8899e4e5beb7265ca2d
Vishal Burman <vishal.a.burman23@gmail.com>
2024-04-06 18:51:21 +0530

    miner.py: Add embedder as a Callable to flow

--- pos_neg_miner/miner.py
+++ pos_neg_miner/miner.py
@@ -1,6 +1,9 @@
-from typing import List
+from typing import Callable, List
 
-from utils import validate_queries_and_candidates
+from utils import (
+    validate_queries_and_candidates,
+    validate_queries_and_candidates_embeddings,
+)
 
 
 class Miner:
@@ -9,7 +12,12 @@ class Miner:
     ):
         pass
 
-    def __call__(self, queries: List[str], candidates: List[str]):
+    def __call__(self, queries: List[str], candidates: List[str], embedder: Callable):
         queries, candidates = validate_queries_and_candidates(
             queries=queries, candidates=candidates
+        )
+        queries_embeds = embedder(queries)
+        candidates_embeds = embedder(candidates)
+        validate_queries_and_candidates_embeddings(
+            queries_embeds=queries_embeds, candidates_embeds=candidates_embeds
         )
