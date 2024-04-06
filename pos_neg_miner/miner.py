44db721b8701ed8d9a9601cd83ab2c23dee747d5
Vishal Burman <vishal.a.burman23@gmail.com>
2024-04-06 18:12:47 +0530

    miner.py: Fix flow of Miner

--- pos_neg_miner/miner.py
+++ pos_neg_miner/miner.py
@@ -1,5 +1,9 @@
 from typing import List
+from utils import validate_queries_and_candidates
 
 class Miner:
-    def __init__(self, queries: List[str], candidates: List[str]):
+    def __init__(self,):
         pass
+    
+    def __call__(self, queries: List[str], candidates: List[str]):
+        queries, candidates = validate_queries_and_candidates(queries=queries, candidates=candidates)
