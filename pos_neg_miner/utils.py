ecc077f70c885e41a15ef369956a84d9a7a0f218
Vishal Burman <vishal.a.burman23@gmail.com>
2024-07-07 17:51:07 +0530

    utils.py: Add docstring for sanitate_text, validate_queries_and_candidates and validate_queries_and_candidates utility functions

--- pos_neg_miner/utils.py
+++ pos_neg_miner/utils.py
@@ -5,14 +5,45 @@ from numpy.typing import NDArray
 
 
 def get_normalized_embeddings(embeds: NDArray) -> NDArray:
+    """Normalize embeddings to unit scale so that dot-product range lies in [-1, 1]
+    Args:
+        embeds: Numpy embeddings to be normalized.
+
+    Returns:
+        NDArray: Normalized numpy embeddings
+    """
     return embeds / np.linalg.norm(embeds)
 
 
 def sanitate_text(text: str) -> str:
+    """Sanitized a piece of text for further validation process.
+
+    Args:
+        text: The text string to be sanitized.
+
+    Returns:
+        str: The sanitized string.
+    """
     return text.strip()
 
 
 def validate_queries_and_candidates(queries: List[str], candidates: List[str]) -> Tuple[List[str], List[str]]:
+    """Sanitizes and validates lists of queries and candidates.
+
+    This function takes in two lists of strings, sanitizes each element by removing duplicates and
+    applying a sanitation function, and then checks that neither list is empty. If either list is
+    empty after sanitization, it raises a ValueError.
+
+    Args:
+        queries (List[str]): A list of query strings to be sanitized and validated.
+        candidates (List[str]): A list of candidate strings to be sanitized and validated.
+
+    Returns:
+        Tuple[List[str], List[str]]: A tuple containing the sanitized lists of queries and candidates.
+
+    Raises:
+        ValueError: If either the queries or candidates list is empty after sanitization.
+    """
     queries = list(map(sanitate_text, set(queries)))
     candidates = list(map(sanitate_text, set(candidates)))
     if not queries or not candidates:
